7865aeb48bed84dd00d6a466b68a226e6f8fce45
kevinnadar <jesikamaraj@gmail.com>
2022-07-01 23:40:28 +0530

    init

--- plugins/channels.py
+++ plugins/channels.py
@@ -10,33 +10,34 @@ from .add_movies import caption
 
 @Client.on_message(filters.chat(CHANNELS_LIST) & filters.media & filters.channel & filters.incoming)
 async def channel_movie_handler(c: Client, m: Message):
-    if m.caption_entities:
-        message = m.caption
-        captions = m.caption_entities
+    if CHANNELS == "True":
+        if m.caption_entities:
+            message = m.caption
+            captions = m.caption_entities
 
-        for count, i in enumerate(await caption(captions)):
-            message = message.replace("👉 Link 🔗", i['url'], 1)
+            for count, i in enumerate(await caption(captions)):
+                message = message.replace("👉 Link 🔗", i['url'], 1)
 
-        id = collection.insert_one(
-            {"caption": message,
-            'title': message.splitlines()[0]
-            }
-        )
+            id = collection.insert_one(
+                {"caption": message,
+                'title': message.splitlines()[0]
+                }
+            )
 
-        reply_markup = InlineKeyboardMarkup(
-        [
-            [InlineKeyboardButton("Delete", callback_data=f"delete#{id.inserted_id }")],
-        ])
-        txt = await c.send_message(
-            chat_id=OWNER_ID,
-            text=f"Post Added Successfully\n\nPost Link: [Click]({m.link})",
-            reply_markup=reply_markup,
-            disable_web_page_preview=True
-        )
-    else:
-        txt = await c.send_message(
-            chat_id=OWNER_ID,
-            text=f"Something went wrong on adding post \n\nPost Link: [Click]({m.id})",
-            reply_markup=reply_markup,
-            disable_web_page_preview=True
-        )
+            reply_markup = InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton("Delete", callback_data=f"delete#{id.inserted_id }")],
+            ])
+            txt = await c.send_message(
+                chat_id=OWNER_ID,
+                text=f"Post Added Successfully\n\nPost Link: [Click]({m.link})",
+                reply_markup=reply_markup,
+                disable_web_page_preview=True
+            )
+        else:
+            txt = await c.send_message(
+                chat_id=OWNER_ID,
+                text=f"Something went wrong on adding post \n\nPost Link: [Click]({m.id})",
+                reply_markup=reply_markup,
+                disable_web_page_preview=True
+            )
