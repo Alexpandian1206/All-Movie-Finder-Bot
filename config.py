c1f0b3086d0a4232158c81dbf74734aa6674a520
Alexpandian1206 <97244736+Alexpandian1206@users.noreply.github.com>
2024-10-23 15:57:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,24 +1,24 @@
 import os
 
-API_ID = os.environ.get('API_ID')
-API_HASH = os.environ.get('API_HASH')
-BOT_TOKEN = os.environ.get('BOT_TOKEN')
-OWNER_ID = int(os.environ.get("OWNER_ID"))
+API_ID = os.environ.get('API_ID', "11450835")
+API_HASH = os.environ.get('API_HASH', "0fadb61feae6ccf016932823bbf1565c")
+BOT_TOKEN = os.environ.get('BOT_TOKEN', "")
+OWNER_ID = int(os.environ.get("OWNER_ID" ""))
 ADMINS = list(int(i) for i in os.environ.get("ADMINS", "").split(" ")) if os.environ.get("ADMINS") else []
 if OWNER_ID not in ADMINS:
     ADMINS.append(OWNER_ID)
-MONGODB = os.environ.get('MONGODB')
-DATABASE_NAME = os.environ.get('DATABASE_NAME') 
-COLLECTION_NAME = os.environ.get('COLLECTION_NAME')
-CHANNELS = os.environ.get('CHANNELS', "False")
+MONGODB = os.environ.get('MONGODB' "")
+DATABASE_NAME = os.environ.get('DATABASE_NAME', "") 
+COLLECTION_NAME = os.environ.get('COLLECTION_NAME' "")
+CHANNELS = os.environ.get('CHANNELS', "True")
 CHANNELS_LIST = list(int(i) for i in os.environ.get("CHANNELS_LIST", "").split(" ")) if os.environ.get("CHANNELS_LIST") else []
-FORCESUB = os.environ.get('FORCESUB', "False")
+FORCESUB = os.environ.get('FORCESUB', "True")
 
 # Other Settings
-UPDATE_CHANNEL =  os.environ.get('UPDATE_CHANNEL')
+UPDATE_CHANNEL =  os.environ.get('UPDATE_CHANNEL' "-1001627812746")
 USERNAME = UPDATE_CHANNEL
 RESULTS_COUNT = int(os.environ.get('RESULT_COUNTS', 10))
-AUTO_DELETE = os.environ.get('AUTO_DELETE', False)
+AUTO_DELETE = os.environ.get('AUTO_DELETE', True)
 AUTO_DELETE_TIME = int(os.environ.get('AUTO_DELETE_TIME', 300))
 IMDB_TEMPLATE = os.environ.get("IMDB_TEMPLATE", "<b>Query: {query}</b> \n‌‌‌‌IMDb Data:\n\n🏷 Title: <a href={url}>{title}</a>\n🎭 Genres: {genres}\n📆 Year: <a href={url}/releaseinfo>{year}</a>\n🌟 Rating: <a href={url}/ratings>{rating}</a> / 10")
 MAX_LIST_ELM = os.environ.get("MAX_LIST_ELM", None)
