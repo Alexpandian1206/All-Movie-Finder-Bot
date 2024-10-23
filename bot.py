aa59c9ef41dafb3d802fac639ae97a0ea865f046
Alex27ak <96329236+Alex27ak@users.noreply.github.com>
2024-10-23 17:02:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,20 @@
-import time
-import os
 from pyrogram import Client
-from config import API_ID, API_HASH, BOT_TOKEN, ADMINS
+from config import *
 
-# Function to synchronize server time
-def sync_time():
-    if os.name == "posix":
-        os.system('sudo ntpdate -s time.nist.gov')
-        print("Time synchronized successfully.")
-    else:
-        print("Please sync time manually on your server.")
+API_ID = API_ID
+API_HASH = API_HASH
+BOT_TOKEN = BOT_TOKEN
+ADMIN = ADMINS
 
-# Sync time before starting the bot
-sync_time()
-
-# Create bot client instance
-bot = Client('droplink_search_bot',
+bot = Client('droplink search bot',
              api_id=API_ID,
              api_hash=API_HASH,
              bot_token=BOT_TOKEN,
              plugins=dict(root="plugins"),
              workers=50,
-             sleep_threshold=10)
+    sleep_threshold=10)
 
 print("Bot Started")
 
-# Running the bot
+# Running the bot.
 bot.run()
